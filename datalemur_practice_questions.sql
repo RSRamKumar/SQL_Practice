c1c2767a00e4ab7c53463bef0abafe4795f8cb37
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-03 16:58:56 +0200

    Update datalemur_practice_questions.sql

--- datalemur_practice_questions.sql
+++ datalemur_practice_questions.sql
@@ -2,3 +2,7 @@
  
 select users.city, count(users.city) from users join trades on trades.user_id = users.user_id where trades.status = 'Completed'
 group by users.city  ORDER BY count(users.city) desc limit 3
+
+
+2. Pharmacy Analytics (Part 1)
+SELECT drug, total_sales - cogs as total_profit FROM pharmacy_sales ORDER BY total_profit desc limit 3;
