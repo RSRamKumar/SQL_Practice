e0b268c76a9ca93d6fef51ec1862b6debab87ea6
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-03 17:11:45 +0200

    Update datalemur_practice_questions.sql

--- datalemur_practice_questions.sql
+++ datalemur_practice_questions.sql
@@ -6,3 +6,6 @@ group by users.city  ORDER BY count(users.city) desc limit 3
 
 2. Pharmacy Analytics (Part 1)
 SELECT drug, total_sales - cogs as total_profit FROM pharmacy_sales ORDER BY total_profit desc limit 3;
+
+3. Pharmacy Analytics (Part 3)
+SELECT manufacturer, CONCAT( '$', ROUND(SUM(total_sales) / 1000000), ' million') as sale FROM pharmacy_sales GROUP BY manufacturer ORDER BY SUM(total_sales) desc ;
