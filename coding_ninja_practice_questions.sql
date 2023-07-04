d51524d9de078cfdab63e1aac8d2a50b9a17d3be
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-04 21:33:37 +0200

    Update coding_ninja_practice_questions.sql

--- coding_ninja_practice_questions.sql
+++ coding_ninja_practice_questions.sql
@@ -1,2 +1,6 @@
 1. Second Highest Salary
 select salary from employee order by salary desc offset 1 limit 1
+
+
+2. Patients With a Condition
+select * from patients where conditions LIKE '%DIAB100%'
