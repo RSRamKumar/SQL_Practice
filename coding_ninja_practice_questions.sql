0925e95173814359d116921fef2088ddcb56b1df
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-06 16:36:05 +0200

    Update coding_ninja_practice_questions.sql

--- coding_ninja_practice_questions.sql
+++ coding_ninja_practice_questions.sql
@@ -4,3 +4,9 @@ select salary from employee order by salary desc offset 1 limit 1
 
 2. Patients With a Condition
 select * from patients where conditions LIKE '%DIAB100%'
+
+3. Customers Who Never Order
+select namecust from customers where namecust   not in
+(select namecust from customers join orders on customers.id = orders.customerid)
+
+--select customers.NameCust as "Customers" from customers where customers.id not in ( select CustomerId from Orders )
