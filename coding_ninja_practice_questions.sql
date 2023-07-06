f61e3b0c57f03e03ea2a98c18a1205586672c5c7
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-06 22:55:14 +0200

    Update coding_ninja_practice_questions.sql

--- coding_ninja_practice_questions.sql
+++ coding_ninja_practice_questions.sql
@@ -16,9 +16,14 @@ select namecust from customers where namecust   not in
 SELECT * FROM city WHERE CountryCode='Marv' AND Population > 100000
 
 5. Fix the Issue
-select lower(TRIM(product_name)) as transformed_product_name,
-TO_CHAR(sale_date::DATE,'YYYY-MM') as transformed_sale_date,
+select lower(TRIM(product_name)) as product_name,
+TO_CHAR(sale_date::DATE,'YYYY-MM') as sale_date,
 count(sale_id) as total
 from sales  
-GROUP BY transformed_product_name, transformed_sale_date
-ORDER BY transformed_product_name,transformed_sale_date )  
+GROUP BY 1, 2
+ORDER BY 1, 2
+
+SELECT LOWER(TRIM(product_name)) AS product_name, SUBSTRING(CAST(sale_date AS VARCHAR(10)),1,7) AS sale_date, 
+COUNT(sale_id) AS total FROM Sales 
+GROUP BY LOWER(TRIM(product_name)),SUBSTRING(CAST(sale_date AS VARCHAR(10)),1,7) 
+ORDER BY 1,2 ASC
