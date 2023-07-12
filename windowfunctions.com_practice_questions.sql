34589fb5ed56c262e6fa40d65b305f8afb4694b0
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-12 20:50:53 +0200

    Update windowfunctions.com_practice_questions.sql

--- windowfunctions.com_practice_questions.sql
+++ windowfunctions.com_practice_questions.sql
@@ -5,3 +5,13 @@ age, sum(weight) as total_weight
 group by age
 having sum(weight) >12
 order by age
+
+
+2.Running Totals
+select  name, sum(weight) over(order by name) running_total_weight
+ from cats order by name
+
+3. Partitioned Running Totals
+select 
+name, breed, sum(weight) over (partition by breed order by name) running_total_weight
+ from cats 
