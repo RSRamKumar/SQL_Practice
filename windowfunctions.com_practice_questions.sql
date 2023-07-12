fe7f2a3505ce4347f3cb3a4a3730311d37d540c7
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-12 21:05:13 +0200

    Update windowfunctions.com_practice_questions.sql

--- windowfunctions.com_practice_questions.sql
+++ windowfunctions.com_practice_questions.sql
@@ -9,16 +9,21 @@ order by age
 
 2.Running Totals
 select  name, sum(weight) over(order by name) running_total_weight
- from cats order by name
+from cats order by name
 
 3. Partitioned Running Totals
 select 
 name, breed, sum(weight) over (partition by breed order by name) running_total_weight
- from cats 
+from cats 
 
 
 4.Examining nearby rows
 select 
 name, weight, 
 avg(weight) over(order by weight ROWS between 1 preceding and 1 following) average_weight
- from cats order by weight
+from cats order by weight
+
+5. Correct Running Total
+select 
+name, sum(weight) over (order by weight DESC ROWS between unbounded preceding and current row) as running_total_weight
+from cats order by running_total_weight
