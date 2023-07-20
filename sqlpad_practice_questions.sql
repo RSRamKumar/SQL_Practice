0f4711f4e223b60bc8195a6b508ed212f06e58f5
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-20 12:09:21 +0200

    Update sqlpad_practice_questions.sql

--- sqlpad_practice_questions.sql
+++ sqlpad_practice_questions.sql
@@ -2,3 +2,9 @@
 
 select store, manager from sales_by_store
 where  total_sales = (select max(total_sales) from sales_by_store)
+
+2. Top 3 movie categories by sales
+SELECT category
+FROM sales_by_film_category
+order by total_sales desc
+LIMIT 3;
