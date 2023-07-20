bd39c63ea5d8a3d28cf09da203cf7a9a319b18d9
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-20 11:52:45 +0200

    Update leetcode_practice_questions.sql

--- leetcode_practice_questions.sql
+++ leetcode_practice_questions.sql
@@ -3,3 +3,9 @@ select class
 from courses
 group by class
 having count(class) >= 5
+
+2. Employee Bonus
+select  name,   bonus.bonus
+from employee
+left join bonus using(empid)
+where bonus<1000 or bonus.bonus is null
