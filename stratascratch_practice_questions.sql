5b0505116cf3d1cd5b25bcd6dd5ac8fc23163bb4
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-20 21:13:08 +0200

    Update stratascratch_practice_questions.sql

--- stratascratch_practice_questions.sql
+++ stratascratch_practice_questions.sql
@@ -4,3 +4,16 @@ select event_name, count(event_name) as event_count from playbook_events
 where device = 'macbook pro'
 group by event_name
 order by event_count desc
+
+2. Find all posts which were reacted to with a heart
+  
+with cte as
+(
+select facebook_posts.post_id, facebook_posts.poster, facebook_posts.post_text, facebook_posts.post_keywords,
+facebook_posts.post_date 
+from facebook_posts join facebook_reactions
+using(post_id)
+where facebook_reactions.reaction = 'heart'
+)
+
+select distinct * from cte
