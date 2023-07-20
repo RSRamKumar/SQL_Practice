9f82512ff647adb621df169a8bb42bd64bddd9e4
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-20 21:18:31 +0200

    Update stratascratch_practice_questions.sql

--- stratascratch_practice_questions.sql
+++ stratascratch_practice_questions.sql
@@ -17,3 +17,8 @@ where facebook_reactions.reaction = 'heart'
 )
 
 select distinct * from cte
+
+(or)
+select distinct  facebook_posts.*
+from facebook_posts   join facebook_reactions  
+on facebook_posts.post_id = facebook_reactions.post_id and facebook_reactions.reaction = 'heart'
