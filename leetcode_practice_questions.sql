4f34998198141713d0569cd1d5067f7fd2b91e4a
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-20 15:22:03 +0200

    Update leetcode_practice_questions.sql

--- leetcode_practice_questions.sql
+++ leetcode_practice_questions.sql
@@ -9,3 +9,20 @@ select  name,   bonus.bonus
 from employee
 left join bonus using(empid)
 where bonus<1000 or bonus.bonus is null
+
+3. Movie Rating
+(
+select   users.name as results
+from movierating join users using(user_id)
+group by user_id
+order by  count(*) desc, name
+limit 1 
+)
+union  all
+(
+ select movies.title as results from movierating join movies
+ using(movie_id)
+ where  EXTRACT(YEAR from movierating.created_at) = '2020' and EXTRACT(MONTH from movierating.created_at) = '2'
+ group by movies.title
+ order by avg(movierating.rating) desc, movies.title limit 1
+)
