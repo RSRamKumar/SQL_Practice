a9111847d2b53ac5c7d2dc5b0f3084ccad381843
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-07-31 14:20:25 +0200

    Update stratascratch_practice_questions.sql

--- stratascratch_practice_questions.sql
+++ stratascratch_practice_questions.sql
@@ -280,5 +280,16 @@ FROM
     worker
 WHERE
     department = 'Admin' AND EXTRACT(month from joining_date) >= 4
+
+23. Customer Details
+SELECT
+    customers.first_name, customers.last_name, customers.city, orders.order_details
+FROM
+    customers
+LEFT JOIN
+    orders
+ON
+    customers.id = orders.cust_id
+ORDER BY customers.first_name, orders.order_details
     
 
