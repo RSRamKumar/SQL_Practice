099277a7cebb99d919a8212e1f3f4ba46a937be8
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-05 19:02:46 +0200

    Update leetcode_practice_questions.sql

--- leetcode_practice_questions.sql
+++ leetcode_practice_questions.sql
@@ -99,3 +99,15 @@ round(sum(CASE WHEN order_date = customer_pref_delivery_date  then 1.0 else 0 en
  ( count(*) ) * 100,2)  
  AS immediate_percentage 
 from delivery_rank_data where delivery_rank = 1 
+
+(or)
+
+Select 
+    round(sum(CASE WHEN order_date = customer_pref_delivery_date  then 1.0 else 0 end )/
+   ( count(*) ) * 100,2)  AS immediate_percentage 
+from Delivery
+where (customer_id, order_date) in (
+  Select customer_id, min(order_date) 
+  from Delivery
+  group by customer_id
+)
