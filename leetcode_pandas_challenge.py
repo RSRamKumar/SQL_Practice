ba44c32e0014ff77d48f233e7ce878b148ef0c13
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-06 19:40:01 +0200

    Update leetcode_pandas_challenge.py

--- leetcode_pandas_challenge.py
+++ leetcode_pandas_challenge.py
@@ -4,3 +4,9 @@ import pandas as pd
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     big_countries_df  = world[(world['area'] >= 3000000 ) | (world['population']  >= 25000000)]
     return big_countries_df [['name', 'population', 'area']]
+
+2. Recyclable and Low Fat Products
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')][['product_id']]
