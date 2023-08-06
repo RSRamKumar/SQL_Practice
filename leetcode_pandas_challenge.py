39952090b228dfedd91e6d312c8cab4b4744a5d4
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-06 20:28:19 +0200

    Update leetcode_pandas_challenge.py

--- leetcode_pandas_challenge.py
+++ leetcode_pandas_challenge.py
@@ -10,3 +10,10 @@ import pandas as pd
 
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
     return products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')][['product_id']]
+
+3. Article Views I
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    df = views[views['author_id'] == views['viewer_id']][['author_id']].drop_duplicates().sort_values(by='author_id').rename(columns={'author_id': 'id'})
+    return df
