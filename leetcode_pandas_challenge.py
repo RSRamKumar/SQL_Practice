f3c9a50d6b3dd6525f64c4b893647639ce4f1200
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-07 17:47:49 +0200

    Update leetcode_pandas_challenge.py

--- leetcode_pandas_challenge.py
+++ leetcode_pandas_challenge.py
@@ -119,4 +119,13 @@ def count_rich_customers(store: pd.DataFrame) -> pd.DataFrame:
        
     }) 
 
+(or)
+import pandas as pd
+
+def count_rich_customers(store: pd.DataFrame) -> pd.DataFrame:
+
+   return  pd.DataFrame (
+       store[store['amount'].gt(500) ][['customer_id']].nunique(),
+       columns = ['rich_count']
+   )
   
