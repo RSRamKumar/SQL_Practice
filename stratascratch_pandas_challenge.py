32d6ca6446909e4bf2aba8d4d47a10bd61085e91
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-08 18:41:56 +0200

    Update stratascratch_pandas_challenge.py

--- stratascratch_pandas_challenge.py
+++ stratascratch_pandas_challenge.py
@@ -6,3 +6,8 @@ hotel_reviews[ hotel_reviews['hotel_name'] == 'Hotel Arena'][
     ['hotel_name', 'reviewer_score']
     ].groupby(by=['reviewer_score','hotel_name'],  as_index=False) .agg(
         n_reviews = pd.NamedAgg(column="reviewer_score", aggfunc="count"))
+
+(or)
+hotel_reviews[ hotel_reviews['hotel_name'] == 'Hotel Arena'][
+    ['hotel_name', 'reviewer_score']
+    ].groupby(by=['reviewer_score','hotel_name'],  as_index=False) .size().rename(columns={'size': 'n_reviews'})
