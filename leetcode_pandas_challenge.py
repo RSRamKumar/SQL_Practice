6e78f23f8e08ebc1e7d4fe7c09dda6ce45c14093
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-08 12:08:19 +0200

    Update leetcode_pandas_challenge.py

--- leetcode_pandas_challenge.py
+++ leetcode_pandas_challenge.py
@@ -226,4 +226,15 @@ def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
         return pd.melt(
         products, id_vars='product_id', var_name='store', value_name='price'
     ).dropna()
- 
+
+18. Daily Leads and Partners
+import pandas as pd
+
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+
+    return daily_sales.groupby(by=['date_id', 'make_name'], as_index=False
+    ).nunique().rename(columns={
+        'lead_id': 'unique_leads',
+        'partner_id': 'unique_partners',
+    })
+    
