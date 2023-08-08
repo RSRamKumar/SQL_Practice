ce13a4806f3184854882d5277fdc1e26d374ad3b
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-08 18:37:50 +0200

    Update stratascratch_pandas_challenge.py

--- stratascratch_pandas_challenge.py
+++ stratascratch_pandas_challenge.py
@@ -1,2 +1,8 @@
 1. Admin Department Employees Beginning in April or Later
 worker[(worker.department == 'Admin') & (worker.joining_date.dt.month >= 4)] 
+
+2. Reviews of Hotel Arena
+hotel_reviews[ hotel_reviews['hotel_name'] == 'Hotel Arena'][
+    ['hotel_name', 'reviewer_score']
+    ].groupby(by=['reviewer_score','hotel_name'],  as_index=False) .agg(
+        n_reviews = pd.NamedAgg(column="reviewer_score", aggfunc="count"))
