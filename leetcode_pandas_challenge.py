380247dee28b8c8caa19ba7ca4b459f303dbe783
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-11 19:05:28 +0200

    Update leetcode_pandas_challenge.py

--- leetcode_pandas_challenge.py
+++ leetcode_pandas_challenge.py
@@ -311,3 +311,9 @@ def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFram
    
 (or)
 customers[~ customers.id.isin(orders.customerId)][['name']].rename(columns={'name': 'Customers'}
+
+(or)
+customers.rename(columns = {'name':'Customers'}, inplace=True)
+return customers.loc[~customers.id.isin(orders.customerId), ['Customers']]
+   
+                                                                   
