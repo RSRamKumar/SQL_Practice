336fd702381389fd215faac8764353ee2efbe6b8
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-12 22:31:06 +0200

    Update leetcode_practice_questions.sql

--- leetcode_practice_questions.sql
+++ leetcode_practice_questions.sql
@@ -221,3 +221,40 @@ SELECT
     dense_rank() over( order by score desc)  AS 'rank'
 FROM
     scores
+
+22.
+DELETE p1
+  FROM Person p1,
+       Person p2
+ WHERE p1.Email = p2.Email
+   AND p1.Id > p2.Id
+"""
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+
+=> From Person p1, Person p2 : it would look like:
+1|john@example.com|1|john@example.com
+1|john@example.com|2|bob@example.com 
+1|john@example.com|3|john@example.com
+
+2|bob@example.com|1|john@example.com
+2|bob@example.com|2|bob@example.com
+2|bob@example.com|3|john@example.com
+
+3|john@example.com|1|john@example.com
+3|john@example.com|2|bob@example.com
+3|john@example.com|3|john@example.com
+
+=> From Person p1, Person p2 where p1.email=p2.email and p1.id>p2.id:
+It would look like:
+3|john@example.com|1|john@example.com
+
+Now delete this row's matching row in p1 using p1:  delete p1
+"""
