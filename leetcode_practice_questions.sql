6a4cef1466eecfb9f661f213887a4473aca73551
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-12 21:41:14 +0200

    Update leetcode_practice_questions.sql

--- leetcode_practice_questions.sql
+++ leetcode_practice_questions.sql
@@ -180,3 +180,11 @@ FROM
     employees
 ORDER BY   
     employee_id
+
+18. Fix Names in a Table
+SELECT
+    user_id, CONCAT(UPPER(LEFT(name, 1)),LOWER(SUBSTRING(name, 2))) AS name
+FROM
+    users
+ORDER BY
+    user_id
