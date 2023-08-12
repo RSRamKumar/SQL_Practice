7522848db3fea4bbe14979313dfa63fdc7904f53
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-12 23:22:02 +0200

    Update leetcode_practice_questions.sql

--- leetcode_practice_questions.sql
+++ leetcode_practice_questions.sql
@@ -310,3 +310,14 @@ FROM
     teacher
 GROUP BY
     teacher_id
+
+27.
+SELECT sell_date,
+       count(DISTINCT product) AS num_sold,
+       group_concat(DISTINCT product ORDER BY product ASC SEPARATOR ',') products
+ FROM 
+     activities
+ GROUP BY 
+     sell_date
+ ORDER BY 
+     sell_date
