e8545110f4fdaebb3133e7d4822039dcc3f3b03a
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-16 22:23:19 +0200

    Update leetcode_practice_questions.sql

--- leetcode_practice_questions.sql
+++ leetcode_practice_questions.sql
@@ -492,3 +492,6 @@ ORDER BY
 select x,y,z ,
 case when x+y > z and x+z >y and y+z > x then 'Yes' else 'No' end as triangle
 from triangle
+
+(or)
+SELECT *, IF(x+y>z and y+z>x and z+x>y, "Yes", "No") as triangle FROM Triangle
