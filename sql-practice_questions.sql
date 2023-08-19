cb26e3015e152e6d5dac8f53a3886bba0ec7e897
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-19 20:41:35 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -29,3 +29,8 @@ where weight between 100 and 120
 UPDATE patients
 SET allergies = 'NKA'
 WHERE allergies IS NULL
+
+6. Show first name and last name concatinated into one column to show their full name.
+SELECT
+  CONCAT(first_name, ' ', last_name) AS full_name
+FROM patients
