ed46f32f76840218be77081ad2a8f0f63105e5c0
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-19 20:39:21 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -17,3 +17,15 @@ where allergies is null
 SELECT first_name
 FROM patients
 where first_name like 'C%'
+
+4.  Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
+SELECT
+  first_name,
+  last_name
+FROM patients
+where weight between 100 and 120
+
+5. Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA
+UPDATE patients
+SET allergies = 'NKA'
+WHERE allergies IS NULL
