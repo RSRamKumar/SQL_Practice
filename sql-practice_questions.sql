bc6bbbb50c80f4328ebee0c4a7aec00e135093ce
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-19 20:50:20 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -58,3 +58,9 @@ where height = (
     select max(height)
     from patients
   )
+
+10. Show all columns for patients who have one of the following patient_ids: 1,45,534,879,1000
+SELECT *
+FROM patients
+where
+  patient_id in (1, 45, 534, 879, 1000)
