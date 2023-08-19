055b6b0fcbc2a7ec38b935610739dccc064fcfb5
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-19 20:36:20 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -1,8 +1,19 @@
 1. Show first name, last name, and gender of patients whose gender is 'M'
-SELECT first_name, last_name, gender FROM patients
+SELECT
+  first_name,
+  last_name,
+  gender
+FROM patients
 where gender = 'M'
 
 2. Show first name and last name of patients who does not have allergies. (null)
-SELECT first_name, last_name  FROM patients
+SELECT
+  first_name,
+  last_name
+FROM patients
 where allergies is null
 
+3. Show first name of patients that start with the letter 'C'
+SELECT first_name
+FROM patients
+where first_name like 'C%'
