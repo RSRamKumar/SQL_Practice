e290391e6e966edcd965a8021b08780916ca1a47
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-20 15:55:35 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -73,4 +73,10 @@ FROM admissions
 SELECT *
 from admissions
 where admission_date = discharge_date
- 
+
+13. Show the patient id and the total number of admissions for patient_id 579.
+SELECT
+  patient_id,
+  count(*) AS total_admissions
+FROM admissions
+WHERE patient_id = 579
