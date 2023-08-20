39da9e0268dbfc61a1343d4988c8e552e0671555
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-20 16:11:48 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -120,3 +120,12 @@ SELECT
   distinct year(birth_date) as birth_year
 FROM patients
 order by birth_year
+
+19. Show unique first names from the patients table which only occurs once in the list.
+
+For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
+
+SELECT first_name
+FROM patients
+group by first_name
+having count(*) = 1
