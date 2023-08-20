a8649d70a1ab7419877cff7497c52c63f0a2a9d4
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-20 16:42:37 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -195,3 +195,11 @@ group by patient_id, diagnosis
 having count(*)>1
 
 26. Show the city and the total number of patients in the city. Order from most to least patients and then by city name ascending.
+
+SELECT
+  city,
+  count(*) as num_patients
+FROM
+  patients
+group by city
+order by num_patients desc , city
