aa6872de6ae14b60134b8829a5fb46b403260817
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-20 15:57:24 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -80,3 +80,8 @@ SELECT
   count(*) AS total_admissions
 FROM admissions
 WHERE patient_id = 579
+
+14. Based on the cities that our patients live in, show unique cities that are in province_id 'NS'?
+SELECT distinct city
+FROM patients
+WHERE province_id = 'NS'
