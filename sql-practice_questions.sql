113428dbf9cf89b846db2645daba2c60f186f044
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-20 16:24:31 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -159,3 +159,10 @@ select
 from patients
   join admissions using(patient_id)
 where diagnosis = 'Dementia'
+
+22. Display every patient's first_name.
+Order the list by the length of each name and then by alphbetically
+
+select first_name 
+from patients
+order by len(first_name), first_name
