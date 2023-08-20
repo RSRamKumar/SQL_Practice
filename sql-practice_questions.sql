c20987a7ff4040eea1878d7577d2572cb0ee26d0
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-20 16:53:58 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -217,3 +217,20 @@ from patients
 where allergies is not null
 group by allergies
 order by total_diagnosis desc
+
+29. Show all patients first_name, last_name, and birth_date who were born in the 1970s decade. Sort the list starting from the earliest birth_date.
+  
+SELECT  first_name, last_name, birth_date
+from patients
+where year(birth_date) >=  1970 and year(birth_date) <= 1979
+order by birth_date
+
+(or)
+SELECT
+  first_name,
+  last_name,
+  birth_date
+FROM patients
+WHERE
+  YEAR(birth_date) BETWEEN 1970 AND 1979
+ORDER BY birth_date ASC;
