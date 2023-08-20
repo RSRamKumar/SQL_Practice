168218e22589548d18be79606a2fd20aa66a72ce
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-20 16:38:19 +0200

    Update sql-practice_questions.sql

--- sql-practice_questions.sql
+++ sql-practice_questions.sql
@@ -189,3 +189,12 @@ ORDER BY
   allergies,  
   first_name ,
   last_name  
+
+25. Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis.
+SELECT
+  patient_id,
+  diagnosis 
+FROM
+  admissions
+group by patient_id, diagnosis
+having count(*)>1
