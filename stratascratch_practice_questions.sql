2a2603cf7e7b54b3659995f0315f0954017e4aca
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-08-25 20:57:52 +0200

    Update stratascratch_practice_questions.sql

--- stratascratch_practice_questions.sql
+++ stratascratch_practice_questions.sql
@@ -475,4 +475,6 @@ GROUP BY
 select *, dense_rank() over (order by sum_n_messages desc) as ranking
 from cte
     
-    
+(or)
+select dense_rank() over(order by sum(n_messages) desc) as ranking, id_guest, sum(n_messages) as sum_n_messages from airbnb_contacts
+group by id_guest order by sum(n_messages) desc
