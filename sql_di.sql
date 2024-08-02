6fc0e71856929e7e3b68a03b556e5443dbaf3907
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2024-08-02 21:06:25 +0200

    Update sql_di.sql

--- sql_di.sql
+++ sql_di.sql
@@ -8,3 +8,12 @@ select  CONCAT(first_name,' ', last_name) renter_name,
 group by   first_name,last_name
 order by total_rentals desc 
 limit 1
+
+
+
+/*
+6. What is the abandon view rate?
+*/
+select count(viewer_id) / (select count(*) from tiktok_fct_views ) * 100.0 as abandon_view_rate
+from tiktok_fct_views
+where viewed_to_completion = 0
