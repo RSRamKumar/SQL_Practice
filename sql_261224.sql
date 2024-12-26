8f6eb986a6e88e47422204cbe2031fe55208dc88
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2024-12-26 18:13:56 +0100

    Update sql_261224.sql

--- sql_261224.sql
+++ sql_261224.sql
@@ -67,11 +67,41 @@ rank() over(partition by state order by population ) as asc_rank
 from city_population
 )
 
-
-
 select state, 
 max(case when desc_rank = 1 then city end) as highest_populated_city,
 max(case when asc_rank = 1 then city end) as lowest_populated_city
 from cte 
 group by state 
- 
+
+
+
+
+--Question Find the number of new and repeating customers each date
+create table customer_orders (
+order_id integer,
+customer_id integer,
+order_date date,
+order_amount integer
+);
+
+insert into customer_orders values(1,100,cast('2022-01-01' as date),2000),(2,200,cast('2022-01-01' as date),2500),(3,300,cast('2022-01-01' as date),2100)
+,(4,100,cast('2022-01-02' as date),2000),(5,400,cast('2022-01-02' as date),2200),(6,500,cast('2022-01-02' as date),2700)
+,(7,100,cast('2022-01-03' as date),3000),(8,400,cast('2022-01-03' as date),1000),(9,600,cast('2022-01-03' as date),3000)
+;
+
+select * from customer_orders;
+
+
+with fvtable as 
+(
+select customer_id,  min(order_date) as first_visit_date
+from customer_orders
+group by customer_id )  
+
+select c.order_date, 
+sum(case when c.order_date = fv.first_visit_date then 1 else 0 end) as first_visit_customers,
+sum(case when c.order_date != fv.first_visit_date then 1 else 0 end) as repeat_customers 
+from customer_orders c 
+join fvtable fv 
+using(customer_id)
+group by c.order_date
